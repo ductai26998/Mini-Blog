69a9b31fb5b282ff595c534657729472ef022063
ductai26998 <ductai26998@gmail.com>
2021-07-30 18:05:34 +0700

    [task][login] set logged in state

--- mini-blog/src/store/index.js
+++ mini-blog/src/store/index.js
@@ -1,11 +1,26 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import firebase from "firebase";
+import "firebase/auth";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    loggedIn: false,
+  },
+  mutations: {
+    changeState(state) {
+      firebase.auth().onAuthStateChanged((user) => {
+        state.loggedIn = !!user;
+      });
+    },
+    logout() {
+      firebase.auth().signOut();
+      this.$router.push("auth/login");
+    }
+  },
   actions: {},
   modules: {},
 });
