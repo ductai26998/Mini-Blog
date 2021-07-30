a5cb34273d02bf38386eb62c30834e1b7cc81022
ductai26998 <ductai26998@gmail.com>
2021-07-30 18:06:24 +0700

    [task][login] setting firebase

--- mini-blog/src/main.js
+++ mini-blog/src/main.js
@@ -2,15 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import {BootstrapVue,IconsPlugin} from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import firebase from 'firebase'
+import firebase from "firebase";
 
 Vue.config.productionTip = false;
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 const firebaseConfig = {
   apiKey: "AIzaSyBc7TJhIpkmV8dIC5WPdvYL8Ufo01CtEak",
@@ -19,12 +19,12 @@ const firebaseConfig = {
   storageBucket: "mini-blog-2k.appspot.com",
   messagingSenderId: "77004920085",
   appId: "1:77004920085:web:70e7849c4e5c359ba9851c",
-  measurementId: "G-QKBT57D8QV"
+  measurementId: "G-QKBT57D8QV",
 };
 // Initialize Firebase
 export var app = firebase.initializeApp(firebaseConfig);
 
-firebase.auth().onAuthStateChanged(() => my_vue)
+firebase.auth().onAuthStateChanged(() => my_vue);
 
 let my_vue = new Vue({
   router,
