604c64a5509c139a7f8365c32583e293050b583c
ductai26998 <ductai26998@gmail.com>
2021-07-30 18:03:26 +0700

    [task][layout] code layout secret and change navbar in default

---
+++ mini-blog/src/layout/secret.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="default-inner">
+        <NavbarSecret/>
+        <slot/>
+        <Subcribe/>
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Footer from "../components/Footer.vue"
+import Subcribe from "../components/Subcribe.vue"
+import NavbarSecret from "../components/NavbarSecret.vue"
+
+export default {
+    components:{
+        Subcribe,
+        Footer,
+        NavbarSecret,
+    },
+}
+</script>
+
+<style>
+
+</style>
