a67d213d60efab29e85c1c77e65cc13bb1cde461
truongthimyduyen27042001 <truongthimyduyen27042001@gmail.com>
2021-07-31 12:18:27 +0700

    ok anh ?

--- mini-blog/src/components/Sliderbar.vue
+++ mini-blog/src/components/Sliderbar.vue
@@ -1,7 +1,33 @@
 <template>
   <div class="sliderbar">
-    <div class="navbar no-radius">
-      <router-link to="/"><h3 class="title">Moose</h3></router-link>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h3 class="title">Moose</h3>
+        </div>
+        <ul class="list-control">
+          <li class="item-control active">
+            <router-link to="/">
+              <i class="fab fa-facebook-f"></i>
+            </router-link>
+          </li>
+          <li class="item-control">
+            <router-link to="/">
+              <i class="fas fa-user"></i>
+            </router-link>
+          </li>
+          <li class="item-control">
+            <router-link to="/">
+              <i class="fas fa-user"></i>
+            </router-link>
+          </li>
+        </ul>
+        <div class="setting">
+          <router-link to="/">
+            <i class="fas fa-cog"></i>
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -18,20 +44,51 @@ export default {
   margin: auto;
 }
 .sliderbar {
-  width: 20vw;
-  background: red;
+  width: 5rem;
+  background: #16202f;
   display: flex;
   overflow: hidden;
   transition: 0.4s ease-in-out;
   display: flex;
+  color: #fff;
+  height: 100vh;
   justify-content: flex-start;
   .title {
     margin-top: 6vh;
     margin-bottom: 6vh;
     font-weight: 600;
     font-family: $font-1;
-    color: #000000!important;
-    text-decoration: none!important;
+    font-size: 1em;
+    color: #fff !important;
+    text-decoration: none !important;
+  }
+  .list-control {
+    list-style: none;
+    padding-top: 1em;
+    .item-control {
+      padding: 6px 0px;
+      margin-bottom: 10px;
+      background: #ef193c;
+      width: 2.5rem;
+      border-radius: 50%;
+      cursor: pointer;
+      a {
+        color: #fff !important;
+      }
+    }
+  }
+  .active {
+  }
+  .setting {
+    margin: 0 auto;
+    margin-top: 55vh;
+    padding: 6px 0px;
+    background: #ef193c;
+    width: 2.5rem;
+    border-radius: 50%;
+    a{
+      color: #fff!important;
+    }
   }
 }
 </style>
