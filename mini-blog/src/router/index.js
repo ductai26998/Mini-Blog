90080ba344355571e10cd6e0d7f29bddf3a7d1e1
truongthimyduyen27042001 <truongthimyduyen27042001@gmail.com>
2021-09-10 15:47:12 +0700

    trang ca nhan

--- mini-blog/src/router/index.js
+++ mini-blog/src/router/index.js
@@ -26,6 +26,11 @@ const routes = [
     component: () => import("../views/Contact.vue"),
   },
   {
+    path: "/personal/:id",
+    name: "Personal",
+    component: () => import("../views/PersonalPage.vue"),
+  },
+  {
     path: "/blogs",
     name: "Blogs",
     component: () => import("../views/Blogs.vue"),
